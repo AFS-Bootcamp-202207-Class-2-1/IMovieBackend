a4c51d8b34b9943a9478114a41ca3a4c000ad44f
“wengwi2” <wengwi2@oocl.com>
2022-08-09 19:19:04 +0800

    fix: getHomePageMovie will return size of six

--- src/main/java/com/pa/imovie/service/MovieService.java
+++ src/main/java/com/pa/imovie/service/MovieService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class MovieService {
@@ -21,12 +22,17 @@ public class MovieService {
     }
 
     public List<HomeMovieInfo> getHomePageMovie() {
-        List<HomeMovieInfo> homeMovieInfos= new ArrayList<>();
         List<Movie> movieList = movieRepository.findAll();
+        List<HomeMovieInfo> homeMovieInfos = new ArrayList<>();
+        AtomicInteger movieCount = new AtomicInteger();
         movieList.stream().forEach((movie) -> {
-            HomeMovieInfo homeMovieInfo = new HomeMovieInfo();
-            BeanUtils.copyProperties(movie, homeMovieInfo);
-            homeMovieInfos.add(homeMovieInfo);
+            if (movieCount.get() < 6){
+                HomeMovieInfo homeMovieInfo = new HomeMovieInfo();
+                BeanUtils.copyProperties(movie, homeMovieInfo);
+                homeMovieInfos.add(homeMovieInfo);
+                movieCount.getAndIncrement();
+            }
+            return;
         });
         return homeMovieInfos;
     }
