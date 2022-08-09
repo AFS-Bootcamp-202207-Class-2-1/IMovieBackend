733bb46ff9ecd37a053dd97574f361c0fe369e6b
WadeQiu <878622383@qq.com>
2022-08-09 09:30:00 +0800

    Test:should_login_fail_and_return_message_the_username_and_password_are_inconsistent_when_call_login_api_given_an_wrong_password

--- src/test/java/com/pa/imovie/controller/UsersControllerTest.java
+++ src/test/java/com/pa/imovie/controller/UsersControllerTest.java
@@ -111,7 +111,27 @@ public class UsersControllerTest {
 
     }
 
-    //the username and password are inconsistent
+    //Test: 登录失败——密码错误
+    @Test
+    void should_login_fail_and_return_message_the_username_and_password_are_inconsistent_when_call_login_api_given_an_wrong_password() throws Exception {
+
+        Users users = new Users(null,"wade","abc","");
+        usersRepository.save(users);
+        Users errorUsers = new Users(null,"wade","error","");
+        String usersJson = new ObjectMapper().writeValueAsString(errorUsers);
+
+        client.perform(MockMvcRequestBuilders.post("/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(usersJson))
+                .andExpect(status().isOk())
+                .andExpect(content().string("the username and password are inconsistent"));
+
+        List<Users> findUsers = usersRepository.findAll();
+        assertThat(findUsers,hasSize(1));
+        assertThat(findUsers.get(0).getUsers_name(), equalTo("wade"));
+        assertThat(findUsers.get(0).getUsers_password(), equalTo("abc"));
+
+    }
 
 
 
