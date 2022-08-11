163ffd1696736c8cc636be267ca3840c7741f8e4
JIAYU LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJI4@OOCL.COM>
2022-08-11 16:22:16 +0800

    refactor:change seatService deleteTicket

--- src/main/java/com/pa/imovie/controller/HelloController.java
+++ src/main/java/com/pa/imovie/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.pa.imovie.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
+
     @GetMapping("/hello")
     public String test(){
         return  "hello";
